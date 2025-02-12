afacd4a222be4cb9d8fbbb4cf482536cdf398541
MychiloCline <84112661+MychiloCline@users.noreply.github.com>
2025-02-12 15:24:20 -0600

    Fix Package.

--- Package.swift
+++ Package.swift
@@ -1,36 +1,34 @@
-// swift-tools-version: 6.0
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "SwiftLlama",
     platforms: [
-        .macOS(.v15),
-        .iOS(.v18),
-        .watchOS(.v11),
-        .tvOS(.v18),
-        .visionOS(.v2)
+        .macOS(.v13),
+        .iOS(.v14),
+        .watchOS(.v9),
+        .tvOS(.v16),
+        .visionOS(.v1)
     ],
     products: [
         .library(name: "SwiftLlama", targets: ["SwiftLlama"]),
     ],
-    cxxSettings: [
-        // Tells the compiler where to find your C++ headers:
-        .headerSearchPath("include"),
-        
-        // If you need to enforce a particular C++ standard, for example:
-        .unsafeFlags(["-std=c++17"])
-    ]
     dependencies: [
         .package(url: "https://github.com/ggerganov/llama.cpp.git", branch: "master")
     ],
     targets: [
-        .target(name: "SwiftLlama", 
-                dependencies: [
-                    .product(name: "llama", package: "llama.cpp")
-                ]),
+        .target(
+            name: "SwiftLlama",
+            dependencies: [
+                .product(name: "llama", package: "llama.cpp")
+            ],
+            cxxSettings: [
+                // Tells the compiler where to find your C++ headers.
+                .headerSearchPath("include"),
+                // Use a particular C++ standard, for example:
+                .unsafeFlags(["-std=c++17"])
+            ]
+        ),
         // .testTarget(name: "SwiftLlamaTests", dependencies: ["SwiftLlama"]),
     ]
 )
-
-
